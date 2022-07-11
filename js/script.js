f2025b6fe0065727e8c88b528de389d60f81d9ca
Michael Wakjira <michaelwakjira999@gmail.com>
2022-07-11 11:44:30 +0300

    finished loopstudio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const width = window.innerWidth;
+
+const navToggle = () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+
+  menu.classList.toggle("hidden");
+};
+
+btn.addEventListener("click", navToggle);
